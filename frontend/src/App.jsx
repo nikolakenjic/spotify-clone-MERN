5a35afa031393c3022d9c2b10f92a39546d318d8
Nikola Kenjic <nikola.nico89@gmail.com>
2024-11-08 15:27:09 +0100

    shadcn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,13 @@
+import { Button } from './components/ui/button';
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>
+    <>
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Hello world!
+      </h1>
+      <Button>Click to me</Button>
+    </>
   );
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
 
-// https://vite.dev/config/
+// Load the .env file from the root
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  define: {
+    'process.env': process.env,
+  },
+});
