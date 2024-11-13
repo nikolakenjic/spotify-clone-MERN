422209f99dba6a4ffd70a7957ef0a6306ca879e2
Nikola Kenjic <nikola.nico89@gmail.com>
2024-11-13 16:21:57 +0100

    homePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,33 @@
 import Topbar from '@/components/layout/Topbar';
-import TestClerk from '@/TestClerk';
+import { ScrollArea } from '@/components/ui/scroll-area';
+// import TestClerk from '@/TestClerk';
 
 const HomePage = () => {
   return (
-    <div>
+    <main className="rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900">
       <Topbar />
-      {/* <TestClerk /> */}
-    </div>
+      <ScrollArea className="h-[calc(100vh-180px)]">
+        <div className="p-4 sm:p-6">
+          <h1 className="text-2xl sm:text-3xl font-bold mb-6">
+            Good afternoon
+          </h1>
+          {/* <FeaturedSection /> */}
+
+          {/* <div className="space-y-8">
+            <SectionGrid
+              title="Made For You"
+              songs={madeForYouSongs}
+              isLoading={isLoading}
+            />
+            <SectionGrid
+              title="Trending"
+              songs={trendingSongs}
+              isLoading={isLoading}
+            />
+          </div> */}
+        </div>
+      </ScrollArea>
+    </main>
   );
 };
 
